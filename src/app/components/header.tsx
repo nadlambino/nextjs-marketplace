cd6e6192357d4ff1d11194374b6e0f0101162919
Ronald Lambino <ronaldlambino0000@gmail.com>
2023-09-24 15:32:00 +0800

    ref: make products page a server component

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -6,33 +6,33 @@ import HeaderMenu from './HeaderItems/HeaderMenu';
 import { AiOutlineShoppingCart, AiOutlineKey } from 'react-icons/ai';
 
 export default async function Header() {
-  const session = await getServerSession(authOptions);
+	const session = await getServerSession(authOptions);
 
-  return (
-    <header>
-      <Link
-        href="/"
-        className="link-icon"
-      >
-        <span className="market">Market</span>
-        <span className="place">place</span>
-      </Link>
-      <ul>
-        <li>
-          <AiOutlineShoppingCart size={20} />
-          <Link href="/cart">Cart</Link>
-        </li>
-        <li>
-          {session && session.user ? (
-            <HeaderMenu />
-          ) : (
-            <>
-              <AiOutlineKey size={20} />
-              <Link href="/auth/signin">Sign In</Link>
-            </>
-          )}
-        </li>
-      </ul>
-    </header>
-  );
+	return (
+		<header className="bg-white shadow-md z-10">
+			<Link
+				href="/"
+				className="link-icon"
+			>
+				<span className="market">Market</span>
+				<span className="place">place</span>
+			</Link>
+			<ul>
+				<li>
+					<AiOutlineShoppingCart size={20} />
+					<Link href="/cart">Cart</Link>
+				</li>
+				<li>
+					{session && session.user ? (
+						<HeaderMenu />
+					) : (
+						<>
+							<AiOutlineKey size={20} />
+							<Link href="/auth/signin">Sign In</Link>
+						</>
+					)}
+				</li>
+			</ul>
+		</header>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        opensans: ['Open Sans', 'sans-serif'],
-      },
-      colors: {
-        dark: '#212121',
-        light: '#ffffff',
-        primary: '#1c87df',
-        secondary: '#d5e6ec',
-        accent: '#0037ff',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				opensans: ['Open Sans', 'sans-serif'],
+			},
+			colors: {
+				dark: '#212121',
+				light: '#ffffff',
+				primary: '#1c87df',
+				secondary: '#d5e6ec',
+				accent: '#0037ff',
+			},
+		},
+	},
+	plugins: [],
 };
